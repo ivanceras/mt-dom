77142b00a2e8fff73a5f4cbc1c2adccaf63b3a90
Jovansonlee Cesar <ivanceras@gmail.com>
2021-08-23 19:00:01 +0800

    Add insert_patches tests

---
+++ tests/insert_patches.rs
@@ -0,0 +1,174 @@
+use mt_dom::{diff::*, patch::*, *};
+
+pub type MyNode = Node<&'static str, &'static str, &'static str, &'static str>;
+#[test]
+fn insert_on_deep_level_keyed() {
+    let old: MyNode = element(
+        "main",
+        vec![attr("key", "container")],
+        vec![
+            element("div", vec![attr("key", "1")], vec![text(0)]),
+            element("div", vec![attr("key", "3")], vec![text(2)]),
+        ],
+    );
+
+    let new: MyNode = element(
+        "main",
+        vec![attr("key", "container")],
+        vec![
+            element("div", vec![attr("key", "1")], vec![text(0)]),
+            element("div", vec![attr("key", "2")], vec![text(1)]),
+            element("div", vec![attr("key", "3")], vec![text(2)]),
+        ],
+    );
+
+    let diff = diff_with_key(&old, &new, &"key");
+
+    dbg!(&diff);
+
+    assert_eq!(
+        diff,
+        vec![InsertNode::new(
+            Some(&"main"),
+            TreePath::start_at(3, vec![0, 1]),
+            &element("div", vec![attr("key", "2")], vec![text(1)])
+        )
+        .into(),]
+    );
+}
+
+#[test]
+fn insert_on_deep_multi_level_level_keyed() {
+    let old: MyNode = element(
+        "main",
+        vec![attr("key", "container")],
+        vec![
+            element("div", vec![attr("key", "1")], vec![text(0)]),
+            element(
+                "div",
+                vec![attr("key", "2")],
+                vec![
+                    element("div", vec![attr("key", "a")], vec![]),
+                    element("div", vec![attr("key", "c")], vec![]),
+                ],
+            ),
+        ],
+    );
+
+    let new: MyNode = element(
+        "main",
+        vec![attr("key", "container")],
+        vec![
+            element("div", vec![attr("key", "1")], vec![text(0)]),
+            element(
+                "div",
+                vec![attr("key", "2")],
+                vec![
+                    element("div", vec![attr("key", "a")], vec![]),
+                    element("div", vec![attr("key", "b")], vec![]),
+                    element("div", vec![attr("key", "c")], vec![]),
+                ],
+            ),
+        ],
+    );
+
+    let diff = diff_with_key(&old, &new, &"key");
+
+    dbg!(&diff);
+
+    assert_eq!(
+        diff,
+        vec![InsertNode::new(
+            Some(&"div"),
+            TreePath::start_at(5, vec![0, 1, 1]),
+            &element("div", vec![attr("key", "b")], vec![])
+        )
+        .into(),]
+    );
+}
+
+#[test]
+fn insert_on_deep_multi_level_keyed_non_keyed_keyed() {
+    let old: MyNode = element(
+        "main",
+        vec![attr("key", "container")],
+        vec![
+            element("div", vec![], vec![text(0)]),
+            element(
+                "div",
+                vec![attr("key", "2")],
+                vec![
+                    element("div", vec![attr("key", "a")], vec![]),
+                    element("div", vec![attr("key", "c")], vec![]),
+                ],
+            ),
+        ],
+    );
+
+    let new: MyNode = element(
+        "main",
+        vec![attr("key", "container")],
+        vec![
+            element("div", vec![], vec![text(0)]),
+            element(
+                "div",
+                vec![attr("key", "2")],
+                vec![
+                    element("div", vec![attr("key", "a")], vec![]),
+                    element("div", vec![attr("key", "b")], vec![]),
+                    element("div", vec![attr("key", "c")], vec![]),
+                ],
+            ),
+        ],
+    );
+
+    let diff = diff_with_key(&old, &new, &"key");
+
+    dbg!(&diff);
+
+    assert_eq!(
+        diff,
+        vec![InsertNode::new(
+            Some(&"div"),
+            TreePath::start_at(5, vec![0, 1, 1]),
+            &element("div", vec![attr("key", "b")], vec![])
+        )
+        .into(),]
+    );
+}
+
+#[test]
+fn insert_on_deep_level_non_keyed_container() {
+    let old: MyNode = element(
+        "main",
+        vec![],
+        vec![
+            element("div", vec![attr("key", "1")], vec![text(0)]),
+            element("div", vec![attr("key", "3")], vec![text(2)]),
+        ],
+    );
+
+    let new: MyNode = element(
+        "main",
+        vec![],
+        vec![
+            element("div", vec![attr("key", "1")], vec![text(0)]),
+            element("div", vec![attr("key", "2")], vec![text(1)]),
+            element("div", vec![attr("key", "3")], vec![text(2)]),
+        ],
+    );
+
+    let diff = diff_with_key(&old, &new, &"key");
+
+    dbg!(&diff);
+
+    assert_eq!(
+        diff,
+        vec![InsertNode::new(
+            Some(&"main"),
+            TreePath::start_at(3, vec![0, 1]),
+            &element("div", vec![attr("key", "2")], vec![text(1)])
+        )
+        .into(),]
+    );
+}
