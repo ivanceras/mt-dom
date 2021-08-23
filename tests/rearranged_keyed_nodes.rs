bbcb96078061409e564bb18cd19d9365245b7246
Jovansonlee Cesar <ivanceras@gmail.com>
2021-08-24 03:40:46 +0800

    Add a failing diff_keyed_elements when children are rearranged

---
+++ tests/rearranged_keyed_nodes.rs
@@ -0,0 +1,66 @@
+use mt_dom::{diff::*, patch::*, *};
+
+pub type MyNode = Node<&'static str, &'static str, &'static str, &'static str>;
+
+#[test]
+fn text_changed_non_keyed() {
+    pretty_env_logger::try_init().ok();
+    let old: MyNode = element(
+        "main",
+        vec![attr("class", "container"), attr("key", "container")],
+        vec![
+            element("div", vec![attr("key", "1")], vec![text("line1")]),
+            element("div", vec![attr("key", "2")], vec![text("line2")]),
+            element("div", vec![attr("key", "3")], vec![text("line3")]),
+        ],
+    );
+
+    let new: MyNode = element(
+        "main",
+        vec![attr("class", "container"), attr("key", "container")],
+        vec![
+            element("div", vec![attr("key", "3")], vec![text("line3")]),
+            element("div", vec![attr("key", "2")], vec![text("line2")]),
+            element("div", vec![attr("key", "1")], vec![text("line1")]),
+        ],
+    );
+
+    let diff = diff_with_key(&old, &new, &"key");
+    dbg!(&diff);
+
+    //FIXME:
+    //
+    // The patch should be: match3, Remove 1, 2 then append 2 and 1
+    assert_eq!(
+        diff,
+        vec![
+            RemoveNode::new(Some(&"main"), TreePath::start_at(1, vec![0, 0]))
+                .into(),
+            RemoveNode::new(Some(&"main"), TreePath::start_at(2, vec![0, 1]))
+                .into(),
+            AppendChildren::new(
+                &"main",
+                TreePath::start_at(0, vec![0]),
+                vec![
+                    (
+                        0,
+                        &element(
+                            "div",
+                            vec![attr("key", "2")],
+                            vec![text("line2")]
+                        )
+                    ),
+                    (
+                        1,
+                        &element(
+                            "div",
+                            vec![attr("key", "1")],
+                            vec![text("line1")]
+                        )
+                    ),
+                ]
+            )
+            .into(),
+        ]
+    );
+}
