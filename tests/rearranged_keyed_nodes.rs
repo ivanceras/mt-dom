ad9b7202430085f87f7d322da0e7fec9f9c70e07
Jovansonlee Cesar <ivanceras@gmail.com>
2022-03-09 12:37:16 +0800

    fix a long standing bug reusing keyed elements when they are rearranged

--- tests/rearranged_keyed_nodes.rs
+++ tests/rearranged_keyed_nodes.rs
@@ -4,7 +4,6 @@ pub type MyNode =
     Node<&'static str, &'static str, &'static str, &'static str, &'static str>;
 
 #[test]
-#[should_panic]
 fn text_changed_non_keyed() {
     pretty_env_logger::try_init().ok();
     let old: MyNode = element(
@@ -30,52 +29,27 @@ fn text_changed_non_keyed() {
     let diff = diff_with_key(&old, &new, &"key");
     dbg!(&diff);
 
-    //FIXME:
-    //
-    // The patch should be: match3, Remove 1, 2 then append 2 and 1
-    //
-    // but the diff was
-    //
-    // [
-    //     Patch::append_children (
-    //         &"main",
-    //         TreePath::new(vec![0]),
-    //         vec![
-    //             element("div",
-    //                  vec![attr("key","1")],
-    //                  vec![leaf("line1") ],
-    //             ),
-    //         ],
-    //     ),
-    //     Patch::remove_node(
-    //         Some(&"div"),
-    //         TreePath::new(vec![0, 0]),
-    //     ),
-    // ]
-
     assert_eq!(
         diff,
         vec![
-            Patch::append_children(
-                &"main",
-                TreePath::new(vec![0]),
-                vec![&element(
-                    "div",
-                    vec![attr("key", "2")],
-                    vec![leaf("line2")]
-                ),]
+            Patch::remove_node(Some(&"div"), TreePath::new(vec![0])),
+            Patch::remove_node(Some(&"div"), TreePath::new(vec![1])),
+            Patch::insert_after_node(
+                Some(&"div"),
+                TreePath::new(vec![2]),
+                vec![
+                    &element(
+                        "div",
+                        vec![attr("key", "2")],
+                        vec![leaf("line2")]
+                    ),
+                    &element(
+                        "div",
+                        vec![attr("key", "1")],
+                        vec![leaf("line1")]
+                    )
+                ]
             ),
-            Patch::append_children(
-                &"main",
-                TreePath::new(vec![0]),
-                vec![&element(
-                    "div",
-                    vec![attr("key", "1")],
-                    vec![leaf("line1")]
-                )]
-            ),
-            Patch::remove_node(Some(&"div"), TreePath::new(vec![0, 0])),
-            Patch::remove_node(Some(&"div"), TreePath::new(vec![0, 1])),
         ]
     );
 }
